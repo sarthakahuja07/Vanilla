fee625f4c8f548c70376932ba35b925c4098a5c7
sarthak <sarthak.ahuja0007@gmail.com>
2021-05-20 15:10:51 +0530

    clock

---
+++ Clock/script.js
@@ -0,0 +1,30 @@
+
+var setClock =function () {
+    var time=new Date();
+    var secTime=time.getSeconds();
+    var minTime=time.getMinutes();
+    var hourTime=time.getHours();
+    
+    console.log(secTime);
+
+
+    var sec=document.querySelector('.second-hand');
+    sec.style.transform=`rotate(${90+secTime*6}deg)`;
+
+    var min=document.querySelector('.min-hand');
+    min.style.transform=`rotate(${90+minTime*6 + (secTime/10)}deg)`;
+
+    var hour=document.querySelector('.hour-hand');
+    hour.style.transform=`rotate(${90+hourTime*6 + (minTime/2)}deg)`;
+
+
+    if(secTime==0){
+        sec.style.transition='none';
+    }else{
+        sec.style.transition='all 0.1s';
+
+    }
+}
+
+
+setInterval(setClock, 1000);
